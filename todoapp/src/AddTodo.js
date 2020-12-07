c9d4c29226e9eb30dc85da4c9cf9984c2cf5fb8d
sophiegiraudo <sophiegiraudo@outlook.com>
2020-12-07 18:56:03 +0800

    Adds files

---
+++ todoapp/src/AddTodo.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+    state = {
+        content: ""
+    }
+    handleChange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            content: ""
+        })
+    }
+    render() {
+        return (
+            <div className="addItem">
+                <br/>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange} value={this.state.content} placeholder="Add Item..."></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo;
